33a7be18bd4e538d0134b8bfe040bd4be13abac7
Joseph Near <jnear@csail.mit.edu>
2014-02-28 17:40:02 -0500

    no change

--- tests.rb
+++ tests.rb
@@ -8,49 +8,50 @@ fnruby {
     
     def test_simple
       x = Cons(1,2)
-      r = match(x,
+      r = match x,
                 Nil => "wrong answer",
-                Cons(a, b) => "right answer! #{a} #{b}")
+                Cons(a, b) => "right answer! #{a} #{b}"
       assert_equal(r, "right answer! 1 2")
 
 
       # nested structures, ignoring bound vars
       x = Cons(1, Cons(2, 3))
-      r = match(x,
+      r = match x,
                 Nil => "wrong answer",
-                Cons(a, Cons(b, c)) => "only used one #{b}")
+                Cons(a, Cons(b, c)) => "only used one #{b}"
       assert_equal(r, "only used one 2")
 
 
       # nested structures and constants, ignoring bound vars
       x = Cons(1, Cons(2, 3))
-      r = match(x,
+      r = match x,
                 Nil => "wrong answer",
-                Cons(1, Cons(b, c)) => "only used one #{b}")
+                Cons(1, Cons(b, c)) => "only used one #{b}"
       assert_equal(r, "only used one 2")
 
 
       # nested structures and WRONG constant
       x = Cons(1, Cons(2, 3))
-      r = match(x,
+      r = match x,
                 Nil => "wrong answer",
-                Cons(2, Cons(b, c)) => "only used one #{b}")
+                Cons(2, Cons(b, c)) => "only used one #{b}"
       assert_equal(r, false)
 
 
       # wrong structure
       x = Cons(1, Nil)
-      r = match(x,
+      r = match x,
                 Nil => "wrong answer",
-                Cons(a, Cons(b, c)) => "only used one #{b}")
+                Cons(a, Cons(b, c)) => "only used one #{b}"
       assert_equal(r, false)
 
 
       # non-adt structures
-      r =  match(1.is_a?(String),
+      r =  match 1.is_a?(String),
                  true => "wrong answer",
-                 false => "right answer")
+                 false => "right answer"
       assert_equal(r, "right answer")
     end
   end
 }
+
